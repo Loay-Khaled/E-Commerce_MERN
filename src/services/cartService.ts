efee4d945fd6e3ce84221bddb0605b9ad8e0480e
Loay <Loay_khaled6@yahoo.com>
2024-08-27 22:01:47 +0300

    Add item to/cart/items POST endpoint

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,27 +1,74 @@
-import { cartModel } from "../models/cartModel"; 
+import { cartModel } from "../models/cartModel";
+import productModel from "../models/productModel";
 
 interface CreateCartForUser {
-    userId: string;
+  userId: string;
 }
 const createCartForUser = async ({ userId }: CreateCartForUser) => {
-    const cart = await cartModel.create({ userId, totalAmount: 0 });
-    await cart.save();
-    return cart;
+  const cart = await cartModel.create({ userId, totalAmount: 0 });
+  await cart.save();
+  return cart;
 };
 
 interface GetActiveCartForUser {
-    userId: string;
+  userId: string;
 }
 
-
 export const getActiveCartForUser = async ({
-     userId, 
-    }: GetActiveCartForUser) => {
-    let cart = await cartModel.findOne({ userId, status: "active" });
+  userId,
+}: GetActiveCartForUser) => {
+  let cart = await cartModel.findOne({ userId, status: "active" });
 
-    if(!cart) {
-        cart = await createCartForUser ({ userId });
-    }
+  if (!cart) {
+    cart = await createCartForUser({ userId });
+  }
 
-    return cart;
+  return cart;
+};
+
+interface addItemToCart {
+  productId: any;
+  quantity: number;
+  userId: string;
+}
+
+export const addItemToCart = async ({
+  productId,
+  quantity,
+  userId,
+}: addItemToCart) => {
+  const cart = await getActiveCartForUser({ userId });
+
+  //Does the item exist in the cart ?
+  const existInCart = cart.items.find(
+    (p) => p.product.toString() === productId
+  );
+
+  if (existInCart) {
+    return { data: " Item already exists in cart", statusCode: 400 };
+  }
+
+  //Fetch the product
+  const product = await productModel.findById(productId);
+
+  if (!product) {
+    return { data: "product not found!", statusCode: 400 };
+  }
+
+  if (product.stock < quantity) {
+    return { data: "Low stock for item", statusCode: 400 };
+  }
+
+  cart.items.push({
+    product: productId,
+    unitPrice: product.price,
+    quantity,
+  });
+
+  // Update the totalAmount for the cart
+  cart.totalAmount += product.price * quantity;
+
+  const updateCart = await cart.save();
+
+  return { data: updateCart, statusCode: 200 };
 };
