7c9e8b277de670448d894d1c4ab3e0af07e5be75
Loay <Loay_khaled6@yahoo.com>
2024-09-05 07:48:43 +0300

    Login Page + Routing

--- frontend/src/context/Auth/AuthProvider.tsx
+++ frontend/src/context/Auth/AuthProvider.tsx
@@ -2,23 +2,26 @@ import { FC, PropsWithChildren, useState } from "react";
 import { AuthContext } from "./AuthContext";
 
 const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
-
-  const [username, setUsername] = useState<string | null>(localStorage.getItem('username'));
-  const [token, setToken] = useState<string | null>(localStorage.getItem('token'))
+  const [username, setUsername] = useState<string | null>(
+    localStorage.getItem("username")
+  );
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token")
+  );
 
   const login = (username: string, token: string) => {
-      setUsername(username);
-      setToken(token)
-      localStorage.setItem('username', username);
-      localStorage.setItem('token', token)
-  }
+    setUsername(username);
+    setToken(token);
+    localStorage.setItem("username", username);
+    localStorage.setItem("token", token);
+  };
 
-  return ( 
-    <AuthContext.Provider value={{ username, token, login}}>
+  const isAuthenticated = !!token;
+  return (
+    <AuthContext.Provider value={{ username, token, login, isAuthenticated }}>
       {children}
     </AuthContext.Provider>
-)
-}
-
+  );
+};
 
 export default AuthProvider;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -8,48 +8,41 @@ import { BASE_URL } from "../constants/baseUrl";
 import { useAuth } from "../context/Auth/AuthContext";
 import { useNavigate } from "react-router-dom";
 
-const RegisterPage = () => {
+const LoginPage = () => {
   const [error, setError] = useState("");
-  const firstNameRef = useRef<HTMLInputElement>(null);
-  const lastNameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
   const navigate = useNavigate();
 
-
   const { login } = useAuth();
   
 
   const onSubmit = async () => {
-    const firstName = firstNameRef.current?.value;
-    const lastName = lastNameRef.current?.value;
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
 
     // Validate the form data
-    if(!firstName || !lastName || !email || !password){
+    if( !email || !password){
         setError('Check submitted data ')
         return;
     }
 
 
     //Make the call to API to create the user
-    const response = await fetch(`${BASE_URL}/user/register`, {
+    const response = await fetch(`${BASE_URL}/user/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        firstName,
-        lastName,
         email,
         password,
       }),
     });
 
     if (!response.ok) {
-      setError("Unable to register user, please try different credentials!");
+      setError("Unable to Login user, please try different credentials!");
       return;
     }
 
@@ -75,7 +68,7 @@ const RegisterPage = () => {
           mt: 4,
         }}
       >
-        <Typography variant="h6">Register New Account</Typography>
+        <Typography variant="h6">Login to Your Account</Typography>
         <Box
           sx={{
             display: "flex",
@@ -87,12 +80,6 @@ const RegisterPage = () => {
             p: 2,
           }}
         >
-          <TextField
-            inputRef={firstNameRef}
-            label="First Name"
-            name="firstName"
-          />
-          <TextField inputRef={lastNameRef} label="Last Name" name="lastName" />
           <TextField inputRef={emailRef} label="Email" name="email" />
           <TextField
             inputRef={passwordRef}
@@ -101,7 +88,7 @@ const RegisterPage = () => {
             name="password"
           />
           <Button onClick={onSubmit} variant="contained">
-            Register
+            Login
           </Button>
           {error && <Typography sx={{ color: "red" }}> {error}</Typography>}
         </Box>
@@ -110,4 +97,4 @@ const RegisterPage = () => {
   );
 };
 
-export default RegisterPage;
+export default LoginPage;
