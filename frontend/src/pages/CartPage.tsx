3777d990f3ce244a37e3d86f0dac1785e0c0c1d7
Loay <Loay_khaled6@yahoo.com>
2024-09-06 12:13:00 +0300

    Cart page layout

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -1,20 +1,108 @@
-import { Box, Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import Container from "@mui/material/Container";
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../constants/baseUrl";
-import { useAuth } from "../context/Auth/AuthContext";
+import { useCart } from "../context/Cart/CartContext";
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
-  const { token } = useAuth();
-  const { cartItems, totalAmount } = useCart();
-  const [error, setError] = useState("");
+  const {
+    cartItems,
+    totalAmount,
+    updateItemInCart,
+    removeItemInCart,
+    clearCart,
+  } = useCart();
+
+  const navigate = useNavigate();
+
+  const handleQuantity = (productId: string, quantity: number) => {
+    if (quantity <= 0) {
+      return;
+    }
+
+    updateItemInCart(productId, quantity);
+  };
+
+  const handleRemoveItem = (productId: string) => {
+    removeItemInCart(productId);
+  };
+
+  const handleCheckout = () => {
+    navigate("/checkout");
+  };
+
+  const renderCartItems = () => (
+    <Box display="flex" flexDirection="column" gap={4}>
+      {cartItems.map((item) => (
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{
+            border: 1,
+            borderColor: "#f2f2f2",
+            borderRadius: 5,
+            padding: 1,
+          }}
+        >
+          <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+            <img src={item.image} width={50} />
+            <Box>
+              <Typography variant="h6">{item.title}</Typography>
+              <Typography>
+                {item.quantity} x {item.unitPrice} EGP
+              </Typography>
+              <Button onClick={() => handleRemoveItem(item.productId)}>
+                Remove Item
+              </Button>
+            </Box>
+          </Box>
+          <ButtonGroup variant="contained" aria-label="Basic button group">
+            <Button
+              onClick={() => handleQuantity(item.productId, item.quantity - 1)}
+            >
+              -
+            </Button>
+            <Button
+              onClick={() => handleQuantity(item.productId, item.quantity + 1)}
+            >
+              +
+            </Button>
+          </ButtonGroup>
+        </Box>
+      ))}
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+        <Typography variant="h4">
+          Total Amount: {totalAmount.toFixed(2)} EGP
+        </Typography>
+        <Button variant="contained" onClick={handleCheckout}>
+          Go To Checkout
+        </Button>
+      </Box>
+    </Box>
+  );
 
   return (
-    <Container sx={{ mt: 2 }}>
-      <Typography variant="h4">My Cart</Typography>
-      {cartItems.map((item) => (
-        <Box>{item.title}</Box>
-      ))}
+    <Container fixed sx={{ mt: 2 }}>
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        sx={{ mb: 2 }}
+      >
+        <Typography variant="h4">My Cart</Typography>
+        <Button onClick={() => clearCart()}>Clear Cart</Button>
+      </Box>
+      {cartItems.length ? (
+        renderCartItems()
+      ) : (
+        <Typography>
+          Cart is empty. Please start shopping and add items first.
+        </Typography>
+      )}
     </Container>
   );
 };
